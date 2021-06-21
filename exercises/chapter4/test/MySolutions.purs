dc56f54b8d53c5dc9b8fcfefdfc6020fea5b272c
Pawel Lisewski <pawel.lisewski@tuta.io>
2021-06-21 18:28:59 +0800

    write countEven

--- exercises/chapter4/test/MySolutions.purs
+++ exercises/chapter4/test/MySolutions.purs
@@ -1,5 +1,23 @@
 module Test.MySolutions where
 
 import Prelude
+import Data.Maybe(fromMaybe)
+import Data.Int(even)
+import Data.Array(head,tail)
 
 -- Note to reader: Add your solutions to this file
+
+isEven :: Int -> Boolean
+isEven n =
+  if n < 0
+    then isEven (-n)
+    else if n == 0
+      then true
+      else not (isEven (n - 1))
+
+countEven :: Array Int -> Int
+countEven [] = 0
+countEven xs = isEvenHead + isEvenTail
+  where oneIfEven n = if even n then 1 else 0
+        isEvenHead = (oneIfEven $ fromMaybe 1 $ head xs)
+        isEvenTail = (countEven $ fromMaybe [] $ tail xs)
