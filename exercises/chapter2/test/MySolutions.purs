196a0c74069fd8569a2f5b5b3df63cb15904fddb
Pawel Lisewski <pawel.lisewski@tuta.io>
2021-06-21 16:23:33 +0800

    write passing test for circleArea

--- exercises/chapter2/test/MySolutions.purs
+++ exercises/chapter2/test/MySolutions.purs
@@ -1,3 +1,10 @@
 module Test.MySolutions where
 
 import Prelude
+import Math (sqrt, pi)
+
+diagonal :: Number -> Number -> Number
+diagonal w h = sqrt (w * w + h * h)
+
+circleArea :: Number -> Number
+circleArea r = pi * r * r
