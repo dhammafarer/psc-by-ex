9a309c5a47c37c491200d3bdf43c4d4bb6fe2ac5
Pawel Lisewski <pawel.lisewski@tuta.io>
2021-06-21 17:38:21 +0800

    wirte findEntryByStreet

--- exercises/chapter3/test/MySolutions.purs
+++ exercises/chapter3/test/MySolutions.purs
@@ -1,5 +1,14 @@
 module Test.MySolutions where
 
 import Prelude
+import Data.AddressBook (Entry, AddressBook)
+import Data.Maybe (Maybe)
+import Data.List (filter, head)
 
 -- Note to reader: Add your solutions to this file
+
+findEntryByStreet :: String -> AddressBook -> Maybe Entry
+findEntryByStreet streetName = head <<< filter filterEntry
+  where filterEntry :: Entry -> Boolean
+        filterEntry entry = entry.address.street == streetName
+
