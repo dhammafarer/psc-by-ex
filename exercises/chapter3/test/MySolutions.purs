f530d5f0ca506fd971af149b14a33a1dd9003aad
Pawel Lisewski <pawel.lisewski@tuta.io>
2021-06-21 17:55:20 +0800

    write isInBook

--- exercises/chapter3/test/MySolutions.purs
+++ exercises/chapter3/test/MySolutions.purs
@@ -3,7 +3,7 @@ module Test.MySolutions where
 import Prelude
 import Data.AddressBook (Entry, AddressBook)
 import Data.Maybe (Maybe)
-import Data.List (filter, head)
+import Data.List (filter, head, null)
 
 -- Note to reader: Add your solutions to this file
 
@@ -12,3 +12,7 @@ findEntryByStreet streetName = head <<< filter filterEntry
   where filterEntry :: Entry -> Boolean
         filterEntry = (eq streetName) <<< _.street <<< _.address
 
+isInBook :: String -> String -> AddressBook -> Boolean
+isInBook f l = not null <<< filter filterEntry
+  where filterEntry :: Entry -> Boolean
+        filterEntry entry = entry.firstName == f && entry.lastName == l
