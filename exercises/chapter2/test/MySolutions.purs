5dc2e3d00671249c01779130aa78ade8c4c6de9f
Pawel Lisewski <pawel.lisewski@tuta.io>
2021-06-21 16:29:03 +0800

    write passing test for leftoverCents

--- exercises/chapter2/test/MySolutions.purs
+++ exercises/chapter2/test/MySolutions.purs
@@ -2,9 +2,13 @@ module Test.MySolutions where
 
 import Prelude
 import Math (sqrt, pi)
+import Data.Int (rem)
 
 diagonal :: Number -> Number -> Number
 diagonal w h = sqrt (w * w + h * h)
 
 circleArea :: Number -> Number
 circleArea r = pi * r * r
+
+leftoverCents :: Int -> Int
+leftoverCents x = rem x 100
