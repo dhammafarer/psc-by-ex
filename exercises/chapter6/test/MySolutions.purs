100a09c053e504df0ea99c31b1f8532b2012e3c0
Pawel Lisewski <pawel.lisewski@tuta.io>
2021-06-23 16:28:37 +0800

    derive eq for complex

--- exercises/chapter6/test/MySolutions.purs
+++ exercises/chapter6/test/MySolutions.purs
@@ -25,3 +25,9 @@ instance Show Complex where
     where
       sign | imaginary < 0.0 = ""
            | otherwise       = "+"
+
+instance Semiring Complex where
+  add (Complex {real: r1, imaginary: i1}) (Complex {real: r2, imaginary: i2}) = Complex {real: (r1+r2), imaginary: (i1+i2)}
+  mul (Complex {real: r1, imaginary: i1}) (Complex {real: r2, imaginary: i2}) = Complex {real: (r1+r2), imaginary: (i1+i2)}
+  zero = Complex {real: 0.0, imaginary: 0.0}
+  one = Complex {real: 1.0, imaginary: 1.0}
