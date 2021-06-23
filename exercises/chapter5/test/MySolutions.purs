abd17e26b1b18b0de95cdcea6ff0e71f94d6ccbb
Pawel Lisewski <pawel.lisewski@tuta.io>
2021-06-23 14:40:21 +0800

    write doubleScaleAndCenter

--- exercises/chapter5/test/MySolutions.purs
+++ exercises/chapter5/test/MySolutions.purs
@@ -1,6 +1,9 @@
 module Test.MySolutions where
 
+import Data.Picture
 import Prelude
+
+import Control.Bind (discard)
 
 factorial :: Int -> Int
 factorial 0 = 1
@@ -21,3 +24,25 @@ pascal n k
 fromSingleton :: forall a. a -> Array a -> a
 fromSingleton _ [b] = b
 fromSingleton a _   = a
+
+circleAtOrigin :: Shape
+circleAtOrigin = Circle origin 10.0
+
+centerShape :: Shape -> Shape
+centerShape (Circle _ r) = Circle origin r
+centerShape (Rectangle _ a b) = Rectangle origin a b
+centerShape line@(Line p1 p2) = Line (p1 - delta) (p2 - delta)
+  where
+    delta = getCenter line
+centerShape (Text _ s) = Text origin s
+
+scaleShape :: Number -> Shape -> Shape
+scaleShape n (Circle c r) = Circle c (r*n)
+scaleShape n (Rectangle c a b) = Rectangle c (a*n) (b*n)
+scaleShape n (Line p1 p2) = Line (p1*scale) (p2*scale)
+  where
+    scale = {x: n, y:n}
+scaleShape _ text = text
+
+doubleScaleAndCenter :: Shape -> Shape
+doubleScaleAndCenter = centerShape <<< scaleShape 2.0
